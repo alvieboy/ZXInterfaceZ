c543bab02fd40b1a836afbbdefa1e7a1e2577575
Alvaro <alvieboy@alvie.com>
2020-03-09 18:49:34 +0000

    Sync ROMs

---
+++ roms/interfacez-sysvars.asm
@@ -0,0 +1,8 @@
+;**********************************
+;** ZX SPECTRUM SYSTEM VARIABLES **
+;**********************************
+P_RAMT		equ	$5C00  
+P_TEMP		equ	$5C02
+
+SCREEN		equ	$4000
+ATTR		equ	$5800
