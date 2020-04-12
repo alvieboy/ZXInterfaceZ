c76354594bc0fb221dc9b168691b4a378916b5ee
Alvaro <alvieboy@alvie.com>
2020-04-12 12:23:23 +0100

    Fix missing file

---
+++ roms/macros.asm
@@ -0,0 +1,23 @@
+ADD_HL_A MACRO
+        ADD	A, L
+    	LD	L, A   ; L = A+L
+    	ADC	A, H   ; A = A+L+H+carry
+    	SUB	L      ; A = H+carry
+    	LD	H, A   ; H = H+carry
+ENDM
+
+ADD_IX_A MACRO
+        ADD	A, IXL
+    	LD	IXL, A   ; L = A+L
+    	ADC	A, IXH   ; A = A+L+H+carry
+    	SUB	IXL      ; A = H+carry
+    	LD	IXH, A   ; H = H+carry
+ENDM
+
+ADD_IY_A MACRO
+        ADD	A, IYL
+    	LD	IYL, A   ; L = A+L
+    	ADC	A, IYH   ; A = A+L+H+carry
+    	SUB	IYL      ; A = H+carry
+    	LD	IYH, A   ; H = H+carry
+ENDM
