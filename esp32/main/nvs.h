58c0aa0bfab2c6cb8477e0d04baaf3bfe9df736d
Alvaro <alvieboy@alvie.com>
2020-07-08 08:41:18 +0100

    Add u16 for NVS

--- esp32/main/nvs.h
+++ esp32/main/nvs.h
@@ -12,14 +12,17 @@ int nvs__fetch_str(const char *key, char *value, unsigned maxlen, const char* de
 
 int nvs__fetch_i32(const char *key, int32_t *value, int32_t def);
 int nvs__fetch_u32(const char *key, uint32_t *value, uint32_t def);
+int nvs__fetch_u16(const char *key, uint16_t *value, uint32_t def);
 int nvs__fetch_u8(const char *key, uint8_t *value, uint8_t def);
 
 uint32_t nvs__u32(const char *key, uint32_t def);
 uint8_t nvs__u8(const char *key, uint8_t def);
+uint16_t nvs__u16(const char *key, uint8_t def);
 int nvs__str(const char *key, char *value, unsigned maxlen, const char* def);
 
 int nvs__set_u32(const char *key, uint32_t val);
 int nvs__set_u8(const char *key, uint8_t val);
+int nvs__set_u16(const char *key, uint16_t val);
 int nvs__set_str(const char *key, const char* val);
 
 int nvs__commit();
