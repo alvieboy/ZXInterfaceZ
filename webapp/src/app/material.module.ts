6f54f397fbf207fcbb6778f308004def658ad409
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-26 11:54:02 +0200

    Created a basic toolbar

--- webapp/src/app/material.module.ts
+++ webapp/src/app/material.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
-    MatSliderModule
+    MatToolbarModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
