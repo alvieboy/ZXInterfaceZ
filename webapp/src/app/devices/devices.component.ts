7c502438a5631e00c8b8b72e31798793eef07c42
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-28 12:11:26 +0200

    Added About and Devices components

--- webapp/src/app/devices/devices.component.ts
+++ webapp/src/app/devices/devices.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.css']
 })
-export class DevicesComponent implements OnInit {
+export class DevicesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'vendor', 'product', 'connected'];
+  devices = new MatTableDataSource<DeviceElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.devices.paginator = this.paginator;
+  }
+
+  constructor(
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
+
+export interface DeviceElement {
+    id: string;
+    vendor: string;
+    product: string;
+    connected: string; // TODO Data types
+}
+
+const ELEMENT_DATA: DeviceElement[] = [
+  { id: "2345:a872", vendor: "Laaa", product: "Plcd", connected: "Yes" },
+]; // TODO Todo call API
--- webapp/src/app/material.module.ts
+++ webapp/src/app/material.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   exports: [
+    MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
