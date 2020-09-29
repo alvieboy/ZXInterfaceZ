a7db540c6f774b549712d59e2fe181bcabefecf4
Alvaro <alvieboy@alvie.com>
2020-09-29 13:26:34 +0100

    Fix VGA modesetting

--- esp32/main/vga.h
+++ esp32/main/vga.h
@@ -1,10 +1,19 @@
 #ifndef __VGA_H__
 #define __VGA_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 #include <inttypes.h>
 
 void vga__init(void);
 int vga__setmode(uint8_t mode);
 int vga__getmode(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
