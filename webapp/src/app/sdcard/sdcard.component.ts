63e4c86b37e33e5544057077548a0c8c9aebd93c
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-30 19:19:00 +0200

    Initial SD Card component

--- webapp/src/app/sdcard/sdcard.component.ts
+++ webapp/src/app/sdcard/sdcard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 @Component({
   selector: 'app-sdcard',
@@ -8,13 +10,40 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class SdcardComponent implements OnInit {
 
+  treeControl = new NestedTreeControl<Vnode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<Vnode>();
+
+  hasChild = (_: number, node: Vnode) => !!node.children && node.children.length > 0;
+
   constructor(
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+    this.dataSource.data = TREE_DATA;
+  }
 
   ngOnInit(): void {
     console.log(this.route.snapshot.queryParamMap);
   }
-
 }
+
+interface Vnode {
+  name: string;
+  extension: string;
+  ftype: string;
+  size: number;
+  children?: Vnode[];
+}
+
+const TREE_DATA: Vnode[] = [
+  { name: 'a-b', extension: '', ftype: 'd', size: 5 },
+  { name: 'c-d', extension: '', ftype: 'd', size: 5, children: [
+    { name: 'Cobble 2.tzx', extension: 'tzx', ftype: 'f', size: 5 },
+    { name: 'Chessfire.tap', extension: 'tap', ftype: 'f', size: 7 },
+  ] },
+  { name: 'e-f', extension: '', ftype: 'd', size: 5, children: [
+    { name: 'Enduro Racer.tzx', extension: 'tzx', ftype: 'f', size: 3 },
+    { name: 'Fighter Bomber.tap', extension: 'tap', ftype: 'f', size: 7 },
+  ] },
+
+]
