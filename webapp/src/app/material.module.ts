8760ec37c82e47a6eb1c565ece6d64e06e91a29b
Rickard von Essen <rickard.von.essen@gmail.com>
2020-09-30 15:39:21 +0200

    Added initial dummy Wifi component

--- webapp/src/app/material.module.ts
+++ webapp/src/app/material.module.ts
@@ -2,24 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatTreeModule,
+    MatTooltipModule
   ],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatTreeModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
