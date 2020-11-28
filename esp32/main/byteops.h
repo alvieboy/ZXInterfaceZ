cb7e4ecf6c4641f3c59224ace4c3a81c0deb3325
Alvaro <alvieboy@alvie.com>
2020-11-28 20:16:07 +0000

    add 32-bit ops

--- esp32/main/byteops.h
+++ esp32/main/byteops.h
@@ -20,6 +20,15 @@ static inline uint32_t extractbe32(const uint8_t *source)
     return ret;
 }
 
+static inline uint32_t extractle32(const uint8_t *source)
+{
+    uint32_t ret = (((uint32_t)source[3]) << 24) +
+        (((uint32_t)source[2]) << 16) +
+        (((uint32_t)source[1]) << 8) +
+        ((uint32_t)source[0]);
+    return ret;
+}
+
 static inline uint16_t extractbe16(const uint8_t *source)
 {
     uint16_t ret = ((((uint16_t)source[0]) << 8) +
@@ -40,4 +49,13 @@ static inline int putle16_c(uint8_t *dest, uint16_t val)
     *dest++ = val & 0xff;
     *dest++ = val >> 8;
     return 2;
+}
+
+static inline uint8_t *putle32(uint8_t *dest, uint32_t val)
+{
+    *dest++ = val & 0xff;
+    *dest++ = val >> 8;
+    *dest++ = val >> 16;
+    *dest++ = val >> 24;
+    return dest;
 }
