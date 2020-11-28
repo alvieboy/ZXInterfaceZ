156de4bb71ddd5ce23f5786748f4b14d6c8ee12d
Alvaro <alvieboy@alvie.com>
2020-11-28 20:19:47 +0000

    Add scope files

---
+++ esp32/main/scope.c
@@ -0,0 +1,43 @@
+#include "scope.h"
+#include <inttypes.h>
+#include "fpga.h"
+#include "byteops.h"
+
+void scope__start()
+{
+    uint8_t val = 0x80;
+    fpga__write_capture_block(0x0013, &val, 1);
+}
+
+void scope__set_triggers(uint32_t mask,
+                         uint32_t value,
+                         uint32_t edge)
+{
+    uint8_t buf[12];
+    uint8_t *ptr = buf;
+    ptr = putle32(ptr, mask);
+    ptr = putle32(ptr, value);
+    ptr = putle32(ptr, edge);
+
+    fpga__write_capture_block(0x0000, buf, 12);
+}
+
+void scope__get_capture_info(uint32_t *status, uint32_t *counter, uint32_t *trig_address)
+{
+    uint8_t buf[12];
+    fpga__read_capture_block(0x0000, buf, 12);
+    *status = extractle32(&buf[0]);
+    *counter = extractle32(&buf[4]);
+    *trig_address = extractle32(&buf[8]);
+}
+
+void scope__get_capture_data_block64(uint8_t channel, uint8_t offset, uint8_t*dest)
+{
+    unsigned address = (1<<13);
+    if (channel)
+        address  |= (1<<12);
+    // 11 to 0 is RAM individual address.
+    // 64 samples are 256 bytes. Offset is 3 bits.
+    address |= ((unsigned)offset&7)<<8;
+    fpga__read_capture_block(address, dest, 256);
+}
---
+++ esp32/main/scope.h
@@ -0,0 +1,19 @@
+#ifndef __SCOPE_H__
+#define __SCOPE_H__
+
+#include <inttypes.h>
+
+#define SCOPE_WIDTH_BITS 10
+
+#define SCOPE_CAPTURE_LEN (((1<<SCOPE_WIDTH_BITS)-1) & ~0xF)
+
+void scope__start(void);
+void scope__set_triggers(uint32_t mask,
+                         uint32_t value,
+                         uint32_t edge);
+
+void scope__get_capture_info(uint32_t *status, uint32_t *counter, uint32_t *trig_address);
+// Get 64 samples (256 bytes).
+void scope__get_capture_data_block64(uint8_t channel, uint8_t offset, uint8_t*dest);
+
+#endif
