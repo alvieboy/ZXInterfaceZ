1fe6f7812e2d9c9df0b746b27b7545ea97615beb
Alvaro <alvieboy@alvie.com>
2020-12-21 14:10:56 +0000

    interrupt: cleanups

--- esp32/main/spectint.c
+++ esp32/main/spectint.c
@@ -10,14 +10,6 @@
 static xQueueHandle gpio_evt_queue = NULL;
 
 static volatile uint32_t interrupt_count = 0;
-        /*
-static void IRAM_ATTR spectint__isr_handler(void* arg)
-{
-    uint32_t gpio_num = ((uint32_t)(size_t) arg );
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-    interrupt_count++;
-}         */
-
 
 static void IRAM_ATTR spectint__isr_handler(void* arg)
 {
@@ -32,24 +24,16 @@ static void IRAM_ATTR spectint__isr_handler(void* arg)
     gpio_hal_set_level(&_gpio_hal, PIN_NUM_INTACK, 1);
 }
 
-//extern void usb__isr_handler(void*);
-
 void spectint__init()
 {
     gpio_evt_queue = xQueueCreate(8, sizeof(uint32_t));
 
-    //gpio_set_intr_type(PIN_NUM_SPECT_INTERRUPT, GPIO_INTR_NEGEDGE);
-    //gpio_set_intr_type(PIN_NUM_CMD_INTERRUPT, GPIO_INTR_NEGEDGE);
     gpio_set_intr_type(PIN_NUM_CMD_INTERRUPT, GPIO_INTR_LOW_LEVEL);
-    //gpio_set_intr_type(PIN_NUM_USB_INTERRUPT, GPIO_INTR_NEGEDGE);
     interrupt_count = 0;
 
     //install gpio isr service
     gpio_install_isr_service(0);
-    //hook isr handler for specific gpio pin
-    //ESP_ERROR_CHECK(gpio_isr_handler_add(PIN_NUM_SPECT_INTERRUPT, spectint__isr_handler, (void*) PIN_NUM_SPECT_INTERRUPT));
     ESP_ERROR_CHECK(gpio_isr_handler_add(PIN_NUM_CMD_INTERRUPT, spectint__isr_handler, (void*) PIN_NUM_CMD_INTERRUPT));
-    //ESP_ERROR_CHECK(gpio_isr_handler_add(PIN_NUM_USB_INTERRUPT, usb__isr_handler, (void*) PIN_NUM_USB_INTERRUPT));
 }
 
 int spectint__getinterrupt()
