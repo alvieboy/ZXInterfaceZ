2fa02e24c31afe95a104f3776aa7c7ebb71afa59
Alvaro <alvieboy@alvie.com>
2020-12-22 15:58:55 +0000

    add MAC routines

--- esp32/host/esplayer/esp_netif.c
+++ esp32/host/esplayer/esp_netif.c
@@ -46,3 +46,12 @@ esp_err_t esp_netif_get_ip_info(esp_netif_t *netif, esp_netif_ip_info_t *info)
     return 0;
 }
 
+esp_err_t esp_netif_get_mac(esp_netif_t *netif, uint8_t *mac)
+{
+    mac[0] = 0xde;
+    mac[1] = 0xad;
+    mac[2] = 0xbe;
+    mac[3] = 0xef;
+    mac[4] = 0xca;
+    mac[5] = 0xfe;
+}
