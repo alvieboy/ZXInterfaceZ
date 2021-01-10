3d5d6551cc66ee6d645331f24d4310f818757d17
Alvaro <alvieboy@alvie.com>
2021-01-10 15:40:41 +0000

    New events

--- esp32/main/systemevent.c
+++ esp32/main/systemevent.c
@@ -1,6 +1,36 @@
 #include "systemevent.h"
+#include "utils.h"
+#include "log.h"
+
+#define MAX_SYSTEMEVENT_HANDLERS 4
+
+static struct {
+    uint8_t mask;
+    systemevent_handler_t handler;
+    void *user;
+} systemevent_handlers[MAX_SYSTEMEVENT_HANDLERS] = {};
+
+void systemevent__register_handler(uint8_t mask, systemevent_handler_t handler, void *user)
+{
+    int i;
+    for (i=0; i<ARRAY_SIZE(systemevent_handlers);i++) {
+        if (systemevent_handlers[i].mask==0) {
+            systemevent_handlers[i].mask = mask;
+            systemevent_handlers[i].handler = handler;
+            systemevent_handlers[i].user = user;
+            return;
+        }
+    }
+    ESP_LOGE("SYSTEMEVENT","Too many handlers!");
+}
 
 void systemevent__send_event(const systemevent_t *event)
 {
-    systemevent__handleevent(event);
+    int i;
+    for (i=0; i<ARRAY_SIZE(systemevent_handlers);i++) {
+        if ((event->type & systemevent_handlers[i].mask)!=0) {
+            if (systemevent_handlers[i].handler!=NULL)
+                systemevent_handlers[i].handler(event, systemevent_handlers[i].user);
+        }
+    }
 }
--- esp32/main/systemevent.h
+++ esp32/main/systemevent.h
@@ -10,6 +10,7 @@ extern "C" {
 #define SYSTEMEVENT_TYPE_WIFI    (1<<0)
 #define SYSTEMEVENT_TYPE_NETWORK (1<<1)
 #define SYSTEMEVENT_TYPE_USB     (1<<2)
+#define SYSTEMEVENT_TYPE_STORAGE (1<<3)
 
 #define SYSTEMEVENT_WIFI_STATUS_CHANGED 0
 #define SYSTEMEVENT_WIFI_SCAN_COMPLETED 1
@@ -19,10 +20,13 @@ extern "C" {
 #define SYSTEMEVENT_USB_DEVICE_CHANGED 0
 #define SYSTEMEVENT_USB_OVERCURRENT 1
 
+#define SYSTEMEVENT_STORAGE_BLOCKDEV_ATTACH 0
+#define SYSTEMEVENT_STORAGE_BLOCKDEV_DETACH 1
 
 typedef struct {
     uint8_t type;
     uint8_t event;
+    void *ctxdata;
 } systemevent_t;
 
 
@@ -35,7 +39,21 @@ static inline void systemevent__send(uint8_t type, uint8_t event)
     systemevent__send_event(&ev);
 }
 
-extern void systemevent__handleevent(const systemevent_t *event);
+static inline void systemevent__send_with_ctx(uint8_t type, uint8_t event, void*ctx)
+{
+    systemevent_t ev;
+    ev.type = type;
+    ev.event = event;
+    systemevent__send_event(&ev);
+}
+
+typedef void (*systemevent_handler_t)(const systemevent_t *event, void *user);
+
+
+void systemevent__register_handler(uint8_t mask, systemevent_handler_t handler, void *user);
+
+
+//extern void systemevent__handleevent(const systemevent_t *event);
 
 
 #ifdef __cplusplus
