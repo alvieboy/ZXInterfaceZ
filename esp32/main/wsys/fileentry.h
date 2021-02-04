9204e80e52eb1ac2db28b3a74acfe51800af89ca
Alvaro <alvieboy@alvie.com>
2021-02-04 14:36:16 +0000

    Add file entry header

---
+++ esp32/main/wsys/fileentry.h
@@ -0,0 +1,20 @@
+#ifndef __FILEENTRY_H__
+#define __FILEENTRY_H__
+
+#include <string>
+#include <vector>
+#include <inttypes.h>
+
+struct FileEntry {
+public:
+    FileEntry(): m_name("") {}
+    FileEntry(uint8_t flags, const char *str): m_flags(flags), m_name(str) {}
+    uint8_t m_flags;
+    std::string m_name;
+    const char *str() const {return m_name.c_str(); }
+    uint8_t flags() const { return m_flags; }
+};
+
+typedef std::vector<FileEntry> FileEntryList;
+
+#endif
