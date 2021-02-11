e5721f0a2e3b9f2ac3096744ec96c5ac6245a094
Alvaro <alvieboy@alvie.com>
2021-02-11 18:30:42 +0000

    save: use new save method

--- esp32/main/save.h
+++ esp32/main/save.h
@@ -5,8 +5,31 @@
 extern "C" {
 #endif
 
-void save__notify_save_to_tap(const char *path, const char *filename);
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef enum {
+    TAPE_NO_TAPE,
+    TAPE_PHYSICAL,
+    TAPE_TAP,
+    TAPE_TZX
+} tapemode_t;
+
+void save__notify_save_to_tap(void);
 void save__notify_no_save(void);
+void save__set_data_from_header(const uint8_t *data, unsigned size);
+
+const char *save__get_requested_name();
+
+int save__start_save_tap(const char *filename,
+                         bool append);
+
+void save__start_save_physical(void);
+
+bool save__is_saving(void);
+tapemode_t save__get_tape_mode(void);
+
+int save__append_from_extram(uint32_t address, uint16_t datalen);
 
 
 #ifdef __cplusplus
