971a66d3166456f0938536c87dfa907b75c3e087
Alvaro <alvieboy@alvie.com>
2021-02-16 17:56:02 +0000

    vlayout: use new generic layout routines

--- esp32/main/wsys/vlayout.cpp
+++ esp32/main/wsys/vlayout.cpp
@@ -6,47 +6,17 @@ VLayout::VLayout(Widget *parent): Layout(parent)
 
 void VLayout::resizeEvent()
 {
-    unsigned expanded_childs = 0;
-    unsigned expanded_size = 0;
-    unsigned nonexpanded_minimum_size = 0;
+    std::vector<int> sizes;
+    computeLayout(sizes,
+                  &Widget::getMinimumHeight,
+                  LAYOUT_FLAG_VEXPAND,
+                  height());
 
-    if (m_numchilds==0)
-        return;
+    int my = y() + m_border;
 
-    for (int i=0;i<m_numchilds;i++) {
-        if (m_flags[i] & LAYOUT_FLAG_VEXPAND) {
-            expanded_childs++;
-        } else {
-            nonexpanded_minimum_size += m_childs[i]->getMinimumHeight();
-        }
-    }
-
-    expanded_size = height() - nonexpanded_minimum_size;
-
-    unsigned my = y();
-
-    if (expanded_childs) {
-        for (int i=0;i<m_numchilds;i++) {
-            if (m_flags[i] & LAYOUT_FLAG_VEXPAND) {
-                unsigned this_expanded_size = expanded_size / expanded_childs;
-                expanded_childs--;
-                expanded_size -= this_expanded_size;
-                m_childs[i]->resize(m_x, my, m_w, this_expanded_size);
-                my+=this_expanded_size;
-            } else {
-                unsigned size = m_childs[i]->getMinimumHeight();
-                m_childs[i]->resize(m_x, my, m_w,size);
-                my+=size;
-            }
-        }
-    } else {
-        unsigned dy = Widget::height() / m_numchilds;
-
-        for (int i=0;i<m_numchilds;i++) {
-            m_childs[i]->resize(m_x, my, m_w, dy);
-            my+=dy;
-
-        }
+    for(int index=0;index<getNumberOfChildren();index++) {
+        childAt(index)->resize(m_x, my, m_w, sizes[index]);
+        my+=sizes[index]+m_spacing;
     }
 }
 
