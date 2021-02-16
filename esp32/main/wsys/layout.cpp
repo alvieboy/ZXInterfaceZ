bfe2b26d8aca902d872e100da77f8bfb5feb62d5
Alvaro <alvieboy@alvie.com>
2021-02-16 17:51:53 +0000

    layout: add border and generic layout routines

--- esp32/main/wsys/layout.cpp
+++ esp32/main/wsys/layout.cpp
@@ -1,20 +1,96 @@
 #include "layout.h"
+#include <climits>
 
 Layout::Layout(Widget *parent): MultiWidget(parent)
 {
     m_spacing = 0;
-}
-
-void Layout::addChild(Widget *w) {
-    m_flags[m_numchilds] = 0;
-    MultiWidget::addChild(w);
+    m_border = 0;
 }
 
 void Layout::addChild(Widget *w, uint8_t flags) {
-    m_flags[m_numchilds] = flags;
+    m_flags.push_back(flags);
     MultiWidget::addChild(w);
 }
 
 void Layout::drawImpl()
 {
+}
+
+void Layout::setSpacing(uint8_t s)
+{
+    m_spacing = s;
+    redraw();
+}
+
+static int shortest(const std::vector<uint8_t> &flags, uint8_t flag, std::vector<int> &sizes)
+{
+    unsigned index=0;
+    int found_index=-1;
+    int min_size = INT_MAX;
+
+    for (index=0; index<sizes.size();index++) {
+        if (!(flags[index] & flag))
+            continue;
+        if (sizes[index]<min_size) {
+            min_size = sizes[index];
+            found_index = index;
+        }
+    }
+    return found_index;
+}
+
+void Layout::computeLayout(std::vector<int> &sizes,
+                           uint8_t (Widget::*sizefun)(void) const,
+                           uint8_t flag,
+                           int size)
+{
+    if (getNumberOfChildren()<1)
+        return;
+
+    sizes.resize(getNumberOfChildren());
+
+    int avail = size - (m_border*2) - (getNumberOfChildren()-1)*m_spacing;
+
+    WSYS_LOGI("Avail: %d\n", avail);
+    int min = 0;
+    int index=0;
+    for (auto i: childs()) {
+        if (!i->isVisible())
+            continue;
+        int thisminsize = (i->*sizefun)();
+        min+=thisminsize;
+        sizes[index++] = thisminsize;
+    }
+    WSYS_LOGI("Min: %d\n", min);
+    WSYS_LOGI("Avail to expand: %d\n", avail-min);
+    int toexpand = avail-min;
+    while (toexpand>0) {
+        int item = shortest(m_flags, flag, sizes);
+        if (item<0)
+            break;
+        WSYS_LOGI("Expanding item %d (size %d) toexpand=%d\n", item, sizes[item], toexpand);
+        sizes[item]++;
+        toexpand--;
+    }
+    index=0;
+
+    int pos = m_border;
+
+    for (auto i: childs()) {
+        if (!i->isVisible())
+            continue;
+        WSYS_LOGI("Item %d: size %d -> pos %d to %d\n", index, sizes[index], pos, pos+sizes[index]-1);
+        pos+=sizes[index];
+        pos+=m_spacing;
+        index++;
+    }
+}
+
+void Layout::visibilityOrFocusPolicyChanged(Widget *w)
+{
+    if (w!=this) {
+        resizeEvent();
+    }
+    if (m_parent)
+        m_parent->visibilityOrFocusPolicyChanged(w);
 }
--- esp32/main/wsys/layout.h
+++ esp32/main/wsys/layout.h
@@ -2,6 +2,7 @@
 #define __LAYOUT_H__
 
 #include "multiwidget.h"
+#include <vector>
 
 #define LAYOUT_FLAG_VEXPAND (1<<0)
 #define LAYOUT_FLAG_HEXPAND (1<<1)
@@ -14,13 +15,23 @@ public:
     virtual void drawImpl();
 
     virtual void resizeEvent() = 0;
-    virtual void addChild(Widget *w);
-    virtual void addChild(Widget *w, uint8_t flags);
+    virtual void addChild(Widget *w, uint8_t flags=0);
     uint8_t spacing() const { return m_spacing; }
     void setSpacing(uint8_t);
+    uint8_t border() const { return m_border; }
+    void setBorder(uint8_t);
+
+    void computeLayout(std::vector<int> &sizes,
+                       uint8_t (Widget::*sizefun)(void) const,
+                       uint8_t flag,
+                       int size);
+
+    virtual void visibilityOrFocusPolicyChanged(Widget *w);
+
 protected:
-    uint8_t m_flags[MULTIWIDGET_MAX_CHILDS];
+    std::vector<uint8_t> m_flags;
     uint8_t m_spacing;
+    uint8_t m_border;
 };
 
 #endif
