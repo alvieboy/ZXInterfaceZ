83692c1190ab54594430bddd1594cce3d589ee20
Alvaro <alvieboy@alvie.com>
2021-02-16 17:52:40 +0000

    messagebox: use generic buttons

--- esp32/main/wsys/messagebox.cpp
+++ esp32/main/wsys/messagebox.cpp
@@ -4,37 +4,55 @@
 MessageBox::MessageBox(const char *title,uint8_t w, uint8_t h): Dialog(title, w, h)
 {
     m_layout = create<VLayout>();
+    m_buttonlayout = create<HLayout>();
     m_text = create<Label>("");
-    m_close = create<Button>("Close [ENTER]");
-
     setChild(m_layout);
     m_layout->addChild(m_text, LAYOUT_FLAG_VEXPAND);
-    m_layout->addChild(m_close);
+    m_layout->addChild(m_buttonlayout);
+    m_buttonlayout->setSpacing(1);
+}
 
-    m_close->clicked().connect(this, &MessageBox::buttonClicked );
+
+Button *MessageBox::addButton(const char *text, int retval)
+{
+    Button *b  = create<Button>(text);
+    m_buttonlayout->addChild(b, LAYOUT_FLAG_HEXPAND);
+    int index = m_retvals.size();
+    m_retvals.push_back(retval);
+    m_buttons.push_back(b);
+
+    b->clicked().connect(
+                         [this,index]{ this->buttonClicked(index); }
+                        );
+
+    return b;
 }
 
 void MessageBox::show(const char *text, const char *button_text)
 {
     MessageBox *m = create<MessageBox>();
     m->setText(text);
-    if (button_text)
-        m->setButtonText(button_text);
+
+    Button *b  = m->addButton(button_text, 0);
+    if (b && button_text)
+        b->setText(button_text);
+
     m->exec();
     m->destroy();
 }
 
-void MessageBox::buttonClicked()
+void MessageBox::buttonClicked(int index)
 {
     if (m_clicked.connected()) {
-        m_clicked.emit();
+        m_clicked.emit(m_buttons[index], m_retvals[index]);
     } else {
         screen__removeWindow(this);
     }
 }
-
-void MessageBox::setButtonText(const char *text)
+#if 0
+void MessageBox::setResult(int val)
 {
-    m_close->setText(text);
+    Dialog::setResult(val);
+    setVisible(false);
 }
-
+#endif
--- esp32/main/wsys/messagebox.h
+++ esp32/main/wsys/messagebox.h
@@ -1,19 +1,27 @@
 #include "dialog.h"
 #include "vlayout.h"
+#include "hlayout.h"
 #include "label.h"
 #include "button.h"
+
 class MessageBox: public Dialog {
 public:
     MessageBox(const char *title="Message", uint8_t w=28, uint8_t h=8);
 
     static void show(const char *message, const char *button_text=NULL);
-    void setText(const char *text) { m_text->setText(text); }
-    void setButtonText(const char *text);
+    const std::vector<Button*> buttons() const;
+    void setText(const char *c) { m_text->setText(c); }
+    Button* addButton(const char *text, int retval);
+
+//    virtual void setResult(int val) override;
+
 protected:
-    void buttonClicked();
+    void buttonClicked(int index);
 private:
     VLayout *m_layout;
     Label *m_text;
-    Button *m_close;
-    Signal<> m_clicked;
+    HLayout *m_buttonlayout;
+    std::vector<Button*> m_buttons;
+    std::vector<int> m_retvals;
+    Signal<Button*, int> m_clicked;
 };
