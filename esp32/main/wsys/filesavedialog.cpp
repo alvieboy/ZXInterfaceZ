8c154c2fda0ecb97c51de710d72441fb62746dde
Alvaro <alvieboy@alvie.com>
2021-02-16 17:50:07 +0000

    filesavedialog: update filters and layout

--- esp32/main/wsys/filesavedialog.cpp
+++ esp32/main/wsys/filesavedialog.cpp
@@ -7,35 +7,103 @@
 #include "editbox.h"
 #include "fixedlayout.h"
 #include "frame.h"
+#include "label.h"
+#include "button.h"
+#include "hlayout.h"
 
-FileSaveDialog::FileSaveDialog(const char*title, uint8_t w, uint8_t h, uint8_t flags): Dialog(title, w,h)
+FileSaveDialog::FileSaveDialog(const char*title, uint8_t w, uint8_t h,
+                               const FileFilter *filter,
+                               uint8_t flags): Dialog(title, w,h)
 {
+    m_frame = create<Frame>("Browsing", w-2, h-10);
     m_menu = create<FileListMenu>();
+    m_menu->setFilter(filter);
+
     m_editbox = create<EditBox>();
+    // TBD: convert this to VLayout once we fix the layouts...
     m_layout = create<FixedLayout>();
-    m_frame = create<Frame>("Navigator", w-2, h-6);
+
+    m_menu->directoryChanged().connect(this, &FileSaveDialog::onDirectoryChanged);
 
     setChild(m_layout);
 
     m_editbox->setEditable(true);
-    m_layout->addChild(m_editbox, 0, 0, w-2, 2);
+    m_layout->addChild( create<Label>("Name: "), 0, 0, w-2, 1);
+
+    m_layout->addChild(m_editbox, 6, 0, w-2-6, 1);
+
+    m_layout->addChild(m_frame, 0, 3, w-2, h-10);
+
+
+    m_menu->selected().connect( this, &FileSaveDialog::activate );
+
+    Label *label1 = create<Label>("Type:");
+    m_layout->addChild( label1, 0+6, 1, 5, 1);
+
+    m_filetypelabel  = create<Label>(filter->getDescription());
+    m_layout->addChild( m_filetypelabel, 5+1+6, 1, w-2-11-6, 1);
+
+
+    HLayout *button_layout = create<HLayout>();
+
+    Button *button1 = create<Button>("Save");
+    Button *button2 = create<Button>("Cancel");
+
+    button1->clicked().connect( [this]{ this->accept(0); });
+    button2->clicked().connect( static_cast<Dialog*>(this), &Dialog::reject);
+
+    //m_layout->addChild( button1, 2, h-6, 8, 1);
+    //m_layout->addChild( button2, 18, h-6, 8, 1);
+    button_layout->setSpacing(1);
+    button_layout->addChild(button1, LAYOUT_FLAG_HEXPAND);
+    button_layout->addChild(button2, LAYOUT_FLAG_HEXPAND);
+    m_layout->addChild( button_layout, 0, h-6, w-2, 1);
 
     m_frame->setChild(m_menu);
-    m_layout->addChild(m_frame, 0, 3, w-2, h-6);
-    m_menu->selected().connect( this, &FileSaveDialog::activate );
+
+}
+
+void FileSaveDialog::onDirectoryChanged(const char *c)
+{
+    char temp[512];
+    if (c==NULL)
+        c="/";
+    sprintf(temp,"Browsing %s", c);
+    m_frame->setTitle(temp);
 }
 
 void FileSaveDialog::activate(uint8_t index)
 {
+    char file[128];
     WSYS_LOGI("Activate index %d",index);
     if (index==0xff) {
         // Closed, abort.
         //releaseResources();
         //screen__removeWindow(this);
-        setResult(-1);
+        //setResult(-1);
         return;
     }
-    setResult(0);
+    // Extract selection from input.
+    const MenuEntry *entry = m_menu->getEntry();
+    if (!entry)
+        return;
+
+    const char *name = entry->string;
+
+    // Extract extension from filename
+    const char *ext = get_file_extension(name);
+    if (!ext) {
+        // No extension??!?!
+        return;
+    }
+    unsigned namesize = (ext-name)-1;
+
+    strncpy(file, name, namesize);
+    file[namesize] = '\0';
+
+    WSYS_LOGI("File name is: '%s'", file);
+    m_editbox->setText(file);
+    //setResult(0);
 }
 
 int FileSaveDialog::exec()
@@ -57,11 +125,15 @@ FileSaveDialog::~FileSaveDialog()
 
 const char *FileSaveDialog::getSelection() const
 {
-    return m_menu->getSelection();
+    return m_editbox->getText();
 }
 
-void FileSaveDialog::setFilter(uint8_t filter)
+void FileSaveDialog::setFilter(const FileFilter *filter)
 {
     m_menu->setFilter(filter);
     //m_filter = filter;
+}
+void FileSaveDialog::setFileName(const char *c)
+{
+    m_editbox->setText(c);
 }
--- esp32/main/wsys/filesavedialog.h
+++ esp32/main/wsys/filesavedialog.h
@@ -8,19 +8,25 @@ class MenuEntryList;
 class EditBox;
 class FixedLayout;
 class Frame;
+class FileFilter;
+class Label;
 
 class FileSaveDialog: public Dialog
 {
 public:
-    FileSaveDialog(const char*title, uint8_t w, uint8_t h, uint8_t flags=0);
+    FileSaveDialog(const char*title, uint8_t w, uint8_t h, const FileFilter *filter, uint8_t flags=0);
     virtual ~FileSaveDialog();
-    void setFilter(uint8_t filter);
+    void setFilter(const FileFilter*);
+    void setFileName(const char *c);
     virtual int exec() override;
     const char *getSelection() const;
+    void onDirectoryChanged(const char*);
+
 protected:
     void activate(uint8_t index);
     FileListMenu *m_menu;
     EditBox *m_editbox;
     FixedLayout *m_layout;
     Frame *m_frame;
+    Label *m_filetypelabel;
 };
