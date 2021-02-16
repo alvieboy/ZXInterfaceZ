5835eadf7af97a10799ca824f4ffd682d767a641
Alvaro <alvieboy@alvie.com>
2021-02-16 17:52:08 +0000

    label: fix label size

--- esp32/main/wsys/label.cpp
+++ esp32/main/wsys/label.cpp
@@ -1,10 +1,70 @@
 #include "label.h"
+#include <cstring>
+
+static int count_newlines(const char *c)
+{
+    int newline = 0;
+    while (*c) {
+        if (*c=='\n')
+            newline++;
+        c++;
+    }
+    return newline;
+}
 
 Label::Label(const char *text,Widget *parent): Widget(parent), m_text(text)
 {
     m_background = -1;
     m_spacing = 0;
+    m_newlines = count_newlines(text);
     redraw();
+}
+
+void Label::setText(const char *text) {
+    m_text=text;
+    redraw();
+}
+
+
+uint8_t Label::getMinimumHeight() const {
+    return m_newlines+1;
+}
+
+uint8_t Label::getMinimumWidth() const
+{
+    uint8_t size = 0;
+    uint8_t linesize = 0;
+    const char *start = m_text.c_str();
+    do {
+        const char *nl = strchr(start,'\n');
+        if (nl) {
+            linesize = nl-start;
+            if (size<linesize)
+                size=linesize;
+        } else {
+            linesize = strlen(start);
+            if (size<linesize)
+                size=linesize;
+            break;
+        }
+    } while (1);
+    return size + 2*m_spacing;
+}
+
+void Label::drawLines(screenptr_t ptr)
+{
+    const char *start = m_text.c_str();
+    do {
+        const char *nl = strchr(start,'\n');
+        if (nl) {
+            ptr.drawstringn(start, nl-start);
+            start = nl+1;
+            ptr.nextcharline();
+        } else {
+            ptr.drawstring(start);
+            break;
+        }
+    } while (1);
 }
 
 void Label::drawImpl()
@@ -14,7 +74,7 @@ void Label::drawImpl()
     screenptr_t screenptr = m_screenptr;
     if (m_text.size()) {
         screenptr += m_spacing;
-        screenptr.drawstring(m_text.c_str());
+        drawLines(screenptr);
     }
 
     if (m_background>=0) {
--- esp32/main/wsys/label.h
+++ esp32/main/wsys/label.h
@@ -9,14 +9,19 @@ class Label: public Widget
 public:
     Label(const char *text=NULL,Widget *parent=NULL);
     virtual void drawImpl();
-    void setText(const char *text) { m_text=text; redraw(); }
+    void setText(const char *text);
     void setBackground(uint8_t background) { m_background=background; redraw(); }
     void setSpacing(uint8_t spacing) { m_spacing = spacing; redraw(); }
     const char *getText() const { return m_text.c_str(); }
+    virtual uint8_t getMinimumHeight() const override;
+    virtual uint8_t getMinimumWidth() const override;
 private:
+    void drawLines(screenptr_t ptr);
+
     std::string m_text;
     int16_t m_background;
     uint8_t m_spacing;
+    uint8_t m_newlines;
 };
 
 
