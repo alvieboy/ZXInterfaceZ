7e782962b578e6801de98ec92f3eafe0bd4fa4ce
Alvaro <alvieboy@alvie.com>
2021-02-16 17:47:20 +0000

    dialog: fix workings, add accept/reject methods

--- esp32/main/wsys/dialog.cpp
+++ esp32/main/wsys/dialog.cpp
@@ -3,14 +3,13 @@
 
 Dialog::Dialog(const char *title, uint8_t w, uint8_t h): Window(title,w,h)
 {
+    setVisible(false);
+    screen__addWindowCentered(this);
 }
 
 void Dialog::setResult(int val)
 {
     m_result=val;
-    //m_callback(m_cbdata, m_result);
-    // close
-    //screen__removeWindow(this);
     setVisible(false);
 }
 
@@ -19,9 +18,19 @@ int Dialog::exec()
  //   m_cbdata = data;
   //  m_callback = callback;
     WSYS_LOGI( "Displaying dialog!");
-    screen__addWindowCentered(this);
+//    screen__addWindowCentered(this);
     setVisible(true);
     screen__windowLoop(this);
     return m_result;
 }
 
+
+void Dialog::accept(uint8_t retval)
+{
+    setResult(retval);
+}
+
+void Dialog::reject()
+{
+    setResult(-1);
+}
--- esp32/main/wsys/dialog.h
+++ esp32/main/wsys/dialog.h
@@ -8,9 +8,11 @@ class Dialog: public Window
 public:
     Dialog(const char *title, uint8_t w, uint8_t h);
 
-    void setResult(int val);
+    virtual void setResult(int val);
     int result() const { return m_result; }
     virtual int exec();//void (*callback)(void*, int), void*);
+    virtual void accept(uint8_t retval=0);
+    virtual void reject();
 private:
     int m_result;
     //void (*m_callback)(void*, int);
