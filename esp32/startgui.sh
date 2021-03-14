875514da94ef05aff133bb519c008a2b62bf3ff1
Alvaro <alvieboy@alvie.com>
2021-03-14 18:13:06 +0000

    host: add default logging

--- esp32/startgui.sh
+++ esp32/startgui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp -a ../roms/intz.rom spiffs/intz.rom
-LD_LIBRARY_PATH=host:host/QtSpecem exec host/gui/interfacez_gui $1 $2 $3 $4
+LD_LIBRARY_PATH=host:host/QtSpecem exec host/gui/interfacez_gui --log log.txt $1 $2 $3 $4
