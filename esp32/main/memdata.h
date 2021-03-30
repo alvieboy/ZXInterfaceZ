4d98f3c93b025a434230c7a855ec5c7ca661003d
Alvaro <alvieboy@alvie.com>
2021-03-30 09:01:01 +0100

    memdata: indentation fixes

--- esp32/main/memdata.h
+++ esp32/main/memdata.h
@@ -17,7 +17,7 @@ struct memdata_request {
 };
 
 uint8_t memdata__analyse_request(uint16_t address, uint8_t len,
-                              struct memdata_request *out, uint8_t *num_requests);
+                                 struct memdata_request *out, uint8_t *num_requests);
 
 const uint8_t *memdata__retrieve(uint8_t *size);
 void memdata__store(const uint8_t *data, uint8_t size);
