faeddedb2ea179eea041c2776a9bff711702f0fc
Alvaro <alvieboy@alvie.com>
2021-04-05 10:15:39 +0100

    roms: add preliminary syscall table

---
+++ roms/syscall.asm
@@ -0,0 +1,16 @@
+SYSCALLTABLE:
+	DW	0        ; 0
+        DW	0        ; 1
+        DW	0        ; 2
+        DW	0        ; 3
+        DW	0        ; 4
+        DW	0        ; 5
+        DW	0        ; 6
+        DW	0        ; 7
+        DW	0        ; 8
+        DW	0        ; 9
+        DW	0        ; 11
+        DW	0        ; 12
+        DW	0        ; 13
+        DW	0        ; 14
+        DW	0        ; 15
