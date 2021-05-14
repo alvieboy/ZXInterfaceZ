492272729ba859c65fdcbb27be5037b6bf016dd2
Alvaro <alvieboy@alvie.com>
2021-05-14 12:40:41 +0100

    #85: add progress reporter

---
+++ esp32/main/progress.c
@@ -0,0 +1,13 @@
+#include "progress.h"
+
+static const char *progress_text[] = {
+    "debug", "info", "warn", "error"
+};
+
+const char *progress__level_to_text(progress_level_t level)
+{
+    int ilevel = (int)level;
+    if (ilevel<=(int)PROGRESS_LEVEL_ERROR)
+        ilevel=(int)PROGRESS_LEVEL_ERROR;
+    return progress_text[ilevel];
+}
---
+++ esp32/main/progress.h
@@ -0,0 +1,30 @@
+#ifndef __PROGRESS_H__
+#define __PROGRESS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int percent_t;
+
+typedef enum {
+    PROGRESS_LEVEL_DEBUG,
+    PROGRESS_LEVEL_INFO,
+    PROGRESS_LEVEL_WARN,
+    PROGRESS_LEVEL_ERROR
+} progress_level_t;
+
+typedef struct {
+    void (*start)(void*user);
+    void (*report)(void*user, percent_t);
+    void (*report_action)(void*user, percent_t percent, progress_level_t level, const char *);
+    void (*report_phase_action)(void*user, percent_t percent, const char *phase, progress_level_t level, const char *);
+} progress_reporter_t;
+
+const char *progress__level_to_text(progress_level_t level);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
