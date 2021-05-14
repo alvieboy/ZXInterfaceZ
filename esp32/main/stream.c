28b2293bf402dc24f66fef549c80bdab772919db
Alvaro <alvieboy@alvie.com>
2021-05-14 11:46:13 +0100

    #85: stream: add support for websockets

--- esp32/main/stream.c
+++ esp32/main/stream.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include "log.h"
 #include "fileaccess.h"
+#include "firmware_ws.h"
+
+#define STREAM_BUFFER_SIZE 512
 
 struct streamops {
     int (*read)(struct stream *, void *buf, size_t size);
@@ -18,7 +21,10 @@ struct stream {
         httpd_req_t *http_req;
     };
     void (*notify_finished)(httpd_req_t*);
+    uint8_t *buffer;
+    unsigned buffer_low, buffer_high;
     bool nonblock;
+    bool buffered;
 };
 
 
@@ -30,6 +36,10 @@ static int system_nonblock(struct stream *, bool);
 static int httpd_read(struct stream *, void *buf, size_t size);
 static int httpd_close(struct stream *);
 static int httpd_nonblock(struct stream *, bool);
+
+static int httpd_ws_read(struct stream *, void *buf, size_t size);
+static int httpd_ws_close(struct stream *);
+static int httpd_ws_nonblock(struct stream *, bool);
 
 
 static const struct streamops system_stream_ops = {
@@ -46,6 +56,18 @@ static const struct streamops httpd_stream_ops = {
     .nonblock = &httpd_nonblock
 };
 
+static const struct streamops httpd_ws_stream_ops = {
+    .read = &httpd_ws_read,
+    .close = &httpd_ws_close,
+    .seek = NULL,
+    .nonblock = &httpd_ws_nonblock
+};
+
+static struct stream* stream__alloc()
+{
+    struct stream *s = calloc(1, sizeof(struct stream));
+    return s;
+}
 
 
 static int system_read(struct stream *s, void *buf, size_t size)
@@ -98,9 +120,26 @@ static int httpd_read(struct stream *s, void *buf, size_t size)
     return received;
 }
 
+static int httpd_ws_read(struct stream *s, void *buf, size_t size)
+{
+    ESP_LOGI("WSREAD", "read %p", s->http_req);
+
+    return firmware_ws__read(s->http_req, buf, size, !s->nonblock);
+    //return -1;
+}
+
 static int httpd_close(struct stream *s)
 {
     ESP_LOGI("STREAM", "HTTP read finished");
+    if (s->notify_finished) {
+        s->notify_finished(s->http_req);
+    }
+    return 0;
+}
+
+static int httpd_ws_close(struct stream *s)
+{
+    ESP_LOGI("STREAM", "HTTP WS read finished");
     if (s->notify_finished) {
         s->notify_finished(s->http_req);
     }
@@ -128,10 +167,16 @@ int httpd_nonblock(struct stream *s, bool nonblock)
     return 0;
 }
 
+int httpd_ws_nonblock(struct stream *s, bool nonblock)
+{
+    s->nonblock = nonblock;
+    return 0;
+}
+
 
 struct stream *stream__alloc_system(int fd)
 {
-    struct stream *s = malloc(sizeof(struct stream));
+    struct stream *s = stream__alloc();
     if (!s)
         return s;
     s->ops = &system_stream_ops;
@@ -141,7 +186,7 @@ struct stream *stream__alloc_system(int fd)
 
 struct stream *stream__alloc_httpd(httpd_req_t*req, void (*notify_finished)(httpd_req_t*))
 {
-    struct stream *s = malloc(sizeof(struct stream));
+    struct stream *s = stream__alloc();
     if (!s)
         return s;
     s->ops = &httpd_stream_ops;
@@ -151,18 +196,138 @@ struct stream *stream__alloc_httpd(httpd_req_t*req, void (*notify_finished)(http
     return s;
 }
 
+struct stream *stream__alloc_websocket(httpd_req_t*req, void (*notify_finished)(httpd_req_t*))
+{
+    struct stream *s = stream__alloc();
+    if (!s)
+        return s;
+    s->ops = &httpd_ws_stream_ops;
+    s->http_req = req;
+
+    ESP_LOGI("STREAM","req %p", req);
+
+    s->notify_finished = notify_finished;
+    s->nonblock = true;
+    return s;
+}
+
 struct stream *stream__destroy(struct stream *s)
 {
     if (s) {
         s->ops->close(s);
+
+        if (s->buffered) {
+            free(s->buffer);
+        }
         free(s);
     }
     return NULL;
 }
 
+int stream__read_buffered(struct stream *s, void *buf, size_t size)
+{
+    int r = 0;
+    uint8_t *target = (uint8_t *)buf;
+    // First, copy data from internal buffer if we have data available.
+    unsigned avail = s->buffer_high - s->buffer_low;
+    ESP_LOGI("STREAM","Stream buffered read %d buffered %d", size, avail);
+    if (avail) {
+        unsigned tocopy = avail > size ? size : avail;
+        memcpy(target, &s->buffer[s->buffer_low], tocopy);
+        size -= tocopy;
+        target += tocopy;
+        s->buffer_low += tocopy;
+        r = tocopy;
+    }
+#ifdef STREAM_NO_BLOCK_READ
+    if (size>0) {
+        // No data in available in buffer.
+        // Read out size_bytes directly.
+        int lr = s->ops->read(s, target, size);
+        if (lr<0) {
+            if (r>0)
+                return r;
+            return -1; // no buffered bytes and error read.
+        }
+        r+=lr; // Update bytes read.
+
+        if (r>0) {
+            // Read out excess data into the local buffer
+            s->buffer_low = s->buffer_high = 0;
+            lr = s->ops->read(s, s->buffer, STREAM_BUFFER_SIZE);
+            if (lr>0) {
+                s->buffer_high = lr;
+            }
+        }
+
+    }
+#else
+    while (size>0) {
+        // No data in available in buffer.
+        if (size >= STREAM_BUFFER_SIZE) {
+            int lr = s->ops->read(s, target, size);
+            if (lr<0) {
+                if (r>0)
+                    return r;
+                return -1; // no buffered bytes and error read.
+            }
+            r+=lr;
+            size-=lr;
+            target+=lr;
+            s->buffer_high = 0;
+            s->buffer_low = 0;
+            ESP_LOGI("STREAM", "Full block read");
+        } else {
+            ESP_LOGI("STREAM", "Short block read");
+            // Less than block size read.
+            // We still may or may not be able to read the whole
+            // size.
+
+            int lr = s->ops->read(s, s->buffer, STREAM_BUFFER_SIZE);
+
+            if (lr<0) {
+                return -1; // no buffered bytes and error read.
+            }
+
+            unsigned fulfilled = lr>size ? size: lr;
+
+            s->buffer_high = lr;
+            s->buffer_low = fulfilled;
+            memcpy(target, s->buffer, fulfilled);
+
+            r+=fulfilled; // Update bytes read.
+            size-=fulfilled;
+            target+=fulfilled;
+        }
+    }
+#endif
+    ESP_LOGI("STREAM", "Ret %d",r);
+    return r;
+}
+
 int stream__read(struct stream *s, void *buf, size_t size)
 {
-    return s->ops->read(s, buf, size);
+    if (!s->buffered)
+        return s->ops->read(s, buf, size);
+
+    // Buffered read.
+    return stream__read_buffered(s, buf, size);
+}
+
+int stream__set_buffering(struct stream *s, bool enabled)
+{
+    if (enabled && !s->buffered) {
+        s->buffer = malloc(STREAM_BUFFER_SIZE);
+        s->buffer_low = 0;
+        s->buffer_high = 0;
+    }
+    if (!enabled && s->buffered) {
+        free(s->buffer);
+    }
+
+    s->buffered = enabled;
+
+    return 0;
 }
 
 int stream__read_blocking(struct stream *s, void *buf, size_t size)
@@ -170,7 +335,7 @@ int stream__read_blocking(struct stream *s, void *buf, size_t size)
     size_t remain = size;
     uint8_t *target = (uint8_t*)buf;
     do {
-        int r = s->ops->read(s, target, remain);
+        int r = stream__read(s, target, remain);
 
         if (r<0)
             return -1;
--- esp32/main/stream.h
+++ esp32/main/stream.h
@@ -6,6 +6,7 @@ struct stream;
 
 struct stream *stream__alloc_system(int fd);
 struct stream *stream__alloc_httpd(httpd_req_t*, void(*notify_finish)(httpd_req_t*) );
+struct stream *stream__alloc_websocket(httpd_req_t*, void(*notify_finish)(httpd_req_t*) );
 struct stream *stream__destroy(struct stream *);
 
 int stream__read(struct stream *, void *buf, size_t size);
@@ -13,5 +14,6 @@ int stream__read_blocking(struct stream *, void *buf, size_t size);
 bool stream__seekable(struct stream *);
 off_t stream__seek(struct stream *, off_t off, int whence);
 int stream__nonblock(struct stream *, bool enable);
+int stream__set_buffering(struct stream *s, bool enabled);
 
 #endif
