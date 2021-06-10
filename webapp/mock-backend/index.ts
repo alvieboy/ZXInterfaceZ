5e1821d1045752588bd23ecab17936afede3282d
Rickard von Essen <rickard.von.essen@gmail.com>
2021-06-10 12:43:18 +0200

    Webapp: Mock backend use json-server from an Express app to allow more advanced features such as Websockets

--- webapp/mock-backend/index.ts
+++ webapp/mock-backend/index.ts
@@ -1,14 +1,34 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-const app = express();
-const {
-  PORT = 3000,
-} = process.env;
-app.get('/', (req: Request, res: Response) => {
+import * as jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser)
+
+server.use(jsonServer.rewriter({
+  '/req/list\\?path=/': '/_sdcard_root',
+  '/req/list\\?path=/a-b/SCRSHOT': '/_sdcard_a-b_SCRSHOT',
+  '/req/list\\?path=/:path': '/_sdcard_:path',
+  '/req/*': '/$1',
+  '/upload/*': '/$1'
+}));
+server.use(middlewares);
+
+server.get('/foo', (req: Request, res: Response) => {
   res.send({
     message: 'hello YOU',
   });
 });
-app.listen(PORT, () => {
-  console.log('server started at http://localhost:'+PORT);
+
+server.use(router);
+
+const {
+  PORT = 3000,
+} = process.env;
+
+server.listen(PORT, () => {
+  console.log('JSON server started at http://localhost:'+PORT);
 });
