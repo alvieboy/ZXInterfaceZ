b721bbaebc23913fd110fda0746646873d2e2c04
Rickard von Essen <rickard.von.essen@gmail.com>
2021-06-20 10:53:18 +0200

    Webapp: Implement a mock backend built on Express and json-server (#96)
    * Webapp: Mock backend built on Express with hot reloading.
    
    * Webapp: Moved json-server files into new Mock backend dir
    
    * Webapp: Mock backend use json-server from an Express app to allow more advanced features such as Websockets
    
    * Mock backend: working WS endpoint in the jsonServer
    
    * Mock backend: split out WebSocket code
    
    * Mock backend: Complete WebSocket implementation

---
+++ webapp/mock-backend/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import jsonServer from 'json-server';
+import ws from 'ws';
+import expressWs from 'express-ws';
+import { fwUpload } from './src/fw-upload';
+
+const {
+  PORT = 3000,
+} = process.env;
+
+const instance = expressWs(jsonServer.create());
+const server = instance.app;
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser)
+server.use(jsonServer.rewriter({
+  '/req/list\\?path=/': '/_sdcard_root',
+  '/req/list\\?path=/a-b/SCRSHOT': '/_sdcard_a-b_SCRSHOT',
+  '/req/list\\?path=/:path': '/_sdcard_:path',
+  '/req/*': '/$1'
+}));
+server.use(middlewares);
+
+server.get('/foo', (req: Request, res: Response) => {
+  res.send({
+    message: 'hello YOU',
+  });
+});
+
+server.ws('/upload/fwupgrade', fwUpload);
+
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('JSON server started at http://localhost:'+PORT);
+});
---
+++ webapp/mock-backend/src/fw-upload.ts
@@ -0,0 +1,52 @@
+import ws from 'ws';
+import { Request } from 'express';
+import sha1 from 'sha1';
+
+var status = [
+  {
+    action: 'Uploading',
+    level: 'info',
+    percent: 10,
+    phase: 'Initializing',
+  },
+  {
+    action: 'WARNING: Flashing sector 2',
+    level: 'warning',
+    percent: 30,
+    phase: 'Programming OTA executable',
+  },
+  {
+    action: 'ERROR: Flashing sector 7',
+    level: 'error',
+    percent: 60,
+    phase: 'Programming OTA executable',
+  },
+  {
+    action: 'Finalizing',
+    level: 'info',
+    percent: 98,
+    phase: 'Validating',
+  },
+  {
+    action: 'Finalizing',
+    level: 'info',
+    percent: 100,
+    phase: 'Completed',
+  }
+];
+
+var i = 0;
+var buf = new ArrayBuffer(2048);
+
+export function fwUpload(ws: ws, req: Request) {
+  ws.on('message', (msg: any) => {
+    console.log(msg);
+    if (msg === 'START') {
+      console.log('Starting upload');
+    } else if (msg === 'STATUS') {
+      ws.send(JSON.stringify(status[i++ % status.length]));
+    } else { // FW file data
+      console.log(`Recieved chunk of length: ${msg.length}, sha1: ${sha1(msg)}`);
+    }
+  });
+}
