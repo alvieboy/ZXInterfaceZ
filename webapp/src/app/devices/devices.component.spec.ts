6a9c9c87e8faa824830b938d6b41c33d7c41c8fe
Rickard von Essen <rickard.von.essen@gmail.com>
2021-07-09 11:34:45 +0200

    Webapp: Upgraded to Angular an Angular Material 11.0

--- webapp/src/app/devices/devices.component.spec.ts
+++ webapp/src/app/devices/devices.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DevicesComponent } from './devices.component';
 
@@ -6,7 +6,7 @@ describe('DevicesComponent', () => {
   let component: DevicesComponent;
   let fixture: ComponentFixture<DevicesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DevicesComponent ]
     })
--- webapp/src/app/sdcard/sdcard.component.spec.ts
+++ webapp/src/app/sdcard/sdcard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SdcardComponent } from './sdcard.component';
 
@@ -6,7 +6,7 @@ describe('SdcardComponent', () => {
   let component: SdcardComponent;
   let fixture: ComponentFixture<SdcardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SdcardComponent ]
     })
